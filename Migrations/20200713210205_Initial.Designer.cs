3507a2394f01caa1b3afb8dd81a94c8e254ca089
Max Eineder <max.eineder@googlemail.com>
2020-07-14 13:57:02 +0200

    Recreate database migrations. Reinstall angular packages.

--- Migrations/20200713210205_Initial.Designer.cs
+++ Migrations/20200713210205_Initial.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.Migrations
 {
     [DbContext(typeof(BlogPostsContext))]
-    [Migration("20190921114025_Initial")]
+    [Migration("20200713210205_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
+                .HasAnnotation("ProductVersion", "2.2.0-rtm-35687")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
